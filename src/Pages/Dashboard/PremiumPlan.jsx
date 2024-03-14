6b0ba6f8a6f8d7ca6cb1bcb257507fdb43e0eb57
monicaum <mahemonica2001@gmail.com>
2024-03-14 18:05:45 +0530

    finished

--- src/Pages/Dashboard/PremiumPlan.jsx
+++ src/Pages/Dashboard/PremiumPlan.jsx
@@ -1,17 +1,17 @@
 // import React from 'react'
-import { DashboardStyles } from './styles'
-import { Button, Typography } from '@mui/material'
-import Crown from '../../assets/images/crown.png'
-import { Container,  Col, Row } from "react-bootstrap";
-import { NavLink } from 'react-router-dom';
+import { DashboardStyles } from "./styles";
+import { Button, Typography } from "@mui/material";
+import Crown from "../../assets/images/crown.png";
+import { Container, Col, Row } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 // import { Card } from "@mui/material";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import studentIcon from "../../assets/icons/9023992_student_fill_icon.svg";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function PremiumPlan() {
-    
-      
-  return ( 
+  return (
     <DashboardStyles>
-        <div className='premium'>
+      {/* <div className='premium'>
         <Typography style={{fontWeight:700}}>Premium Plans <img src={Crown} height="15px" /></Typography>
        
         <Container  className='mainContainer'>
@@ -85,7 +85,66 @@ export default function PremiumPlan() {
         </Row>
     </Container>
         
-        </div>
+        </div> */}
+
+      <div className="container">
+        <Col xs={12} sm={12} md={6} lg={3} xl={3}>
+          <div className="item">
+            <div className="innerContent">
+              <div>
+                <img src={studentIcon} alt="Description of the image" />
+              </div>
+              <div className="vertical-line"></div>
+              <div style={{ display: "flex" }}>
+                <Typography
+                  style={{
+                    color: "#E4A45A",
+                    fontFamily: "Roboto",
+                    fontSize: "35px",
+                    paddingLeft: "20px",
+                  }}
+                >
+                  20
+                </Typography>
+              </div>
+            </div>
+            <div
+              className="text"
+              style={{ position: "relative", right: "62%" }}
+            >
+              <Typography style={{ fontWeight: "bold" }}>Premium</Typography>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={3} xl={3}>
+          <div className="item">
+            <div className="innerContent">
+              <div>
+                <img src={studentIcon} alt="Description of the image" />
+              </div>
+              <div className="vertical-line"></div>
+              <div style={{ display: "flex" }}>
+                <Typography
+                  style={{
+                    color: "#E4A45A",
+                    fontFamily: "Roboto",
+                    fontSize: "35px",
+                    paddingLeft: "20px",
+                  }}
+                >
+                  20
+                </Typography>
+              </div>
+            </div>
+            <div
+              className="text"
+              style={{ position: "relative", right: "62%",padding:'1vh 3vw' }}
+            >
+              <Typography style={{ fontWeight: "bold" }}>Premium</Typography>
+            </div>
+          </div>
+        </Col>
+      </div>
     </DashboardStyles>
-  )
+  );
 }
